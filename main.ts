02bab8e8c36fecfb696f48d96b43f6cb4927d5f8
ohtAIrobo09tgc <67031867+ohtAIrobo09tgc@users.noreply.github.com>
2020-06-17 12:09:36 +0900

    じゃんけんのプログラム

--- main.ts
+++ main.ts
@@ -1,4 +1,84 @@
-basic.showIcon(IconNames.Yes)
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (じゃんけん == 1 && receivedNumber == 1) {
+        radio.sendString("あいこ")
+    } else if (じゃんけん == 2 && receivedNumber == 1) {
+        radio.sendString("まけ")
+    } else if (じゃんけん == 3 && receivedNumber == 1) {
+        radio.sendString("かち")
+    } else if (じゃんけん == 1 && receivedNumber == 2) {
+        radio.sendString("まけ")
+    } else if (じゃんけん == 2 && receivedNumber == 2) {
+        radio.sendString("あいこ")
+    } else if (じゃんけん == 3 && receivedNumber == 2) {
+        radio.sendString("かち")
+    } else if (じゃんけん == 1 && receivedNumber == 3) {
+        radio.sendString("かち")
+    } else if (じゃんけん == 2 && receivedNumber == 3) {
+        radio.sendString("まけ")
+    } else if (じゃんけん == 3 && receivedNumber == 3) {
+        radio.sendString("あいこ")
+    }
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    radio.sendNumber(1)
+    じゃんけん = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # . # .
+        # . . . #
+        # . . . #
+        `)
+    radio.sendNumber(3)
+    じゃんけん = 3
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "かち") {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    } else if (receivedString == "まけ") {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else if (receivedString == "あいこ") {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        . # . # .
+        . # # # .
+        . . # . .
+        `)
+    radio.sendNumber(2)
+    じゃんけん = 2
+})
+let じゃんけん = 0
+radio.setGroup(10)
+basic.showIcon(IconNames.Yes)
